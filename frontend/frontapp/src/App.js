2fa527cf7776a0767e3476aa1eb3c20a96d4e723
bdobrosielski <bdobrosielski@edu.cdv.pl>
2022-03-30 14:30:01 +0200

    react list of books

--- frontend/frontapp/src/App.js
+++ frontend/frontapp/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 import Login from './components/Login';
+import Books from './components/Books';
 
 function App() {
+
+  const [token, setToken] = useState('')
+
+  const userLogin = (token) => {
+    setToken(token);
+  }
+
   return (
     <div className="App">
         <h1>Main Page</h1>
-        <Login />
+        <Login userLogin={userLogin}/>
+        <Books />
     </div>
   );
 }
--- frontend/frontapp/src/components/Login.js
+++ frontend/frontapp/src/components/Login.js
@@ -27,20 +27,41 @@ class Login extends Component {
     this.setState({credentials: cred})
   }
 
+  signUp = event =>{
+    console.log(this.state.cridentials);
+    fetch('http://127.0.0.1:8000/api/users/', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(this.state.cridentials)
+    })
+    .then(data => data.json())
+    .then(
+      data => {
+        this.props.userLogin(data.token);
+      }
+    ).catch( error => console.log(error))
+  }
+
+  inputChanged = event =>{
+    const cred = this.state.cridentials;
+    cred[event.target.name] = event.target.value;
+    this.setState({credentials: cred})
+  }
+
   render(){
     return (
         <div>
             <h1> Login User </h1>
             <label>
-              username:
               <input type="text" name="username" value={this.state.cridentials.username}
-              onChange={this.inputChanged}/>
-              password:
+              onChange={this.inputChanged} placeholder="username"/>
+              <br/>
               <input type="password" name="password" value={this.state.cridentials.password}
-              onChange={this.inputChanged}/>
+              onChange={this.inputChanged} placeholder="password"/>
+              <br/>
             </label>
             <button onClick={this.login}>Login</button>
-
+           `<button onClick={this.signUp}>SignUp</button>
         </div>
   );
 }
