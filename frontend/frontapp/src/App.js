a0f77c90e2156fd3874cd9b671dc5482a01b24e9
bdobrosielski <bdobrosielski@edu.cdv.pl>
2022-03-31 14:32:16 +0200

    tests

--- frontend/frontapp/src/App.js
+++ frontend/frontapp/src/App.js
@@ -6,16 +6,32 @@ import Books from './components/Books';
 function App() {
 
   const [token, setToken] = useState('')
+  var logginButton, booksButton, logoutButton
 
   const userLogin = (tok) => {
     setToken(tok);
   }
 
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  }
+
+  if (token === '') {
+    logginButton = <Login userLogin={userLogin}/>;
+  } else {
+    logginButton = <div>You are logged</div>;
+    booksButton = <Books token={token}/>;
+    logoutButton = <a href="/" onClick={logout}>logout</a>
+  }
+
   return (
     <div className="App">
         <h1>Main Page</h1>
-        <Login userLogin={userLogin}/>
-        <Books token={token}/>
+        {logoutButton}
+        {logginButton}
+        {booksButton}
+        
     </div>
   );
 }
---
+++ tests/test_create_book.py
@@ -0,0 +1,10 @@
+# import pytest
+# from api.models import Book
+#
+#
+# @pytest.mark.filterwarnings
+# @pytest.mark.django_db
+# def book_model_create():
+#     Book.objects.create_user('title1')
+#     Book.objects.create_user('title2')
+#     assert Book.objects.count() == 2
